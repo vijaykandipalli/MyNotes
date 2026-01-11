544379ad3b353c76e86444bb57bab0602626c08f
vizzu999 <vijayshankarkandipalli@gmail.com>
2026-01-11 10:17:48 +0530

    add fully working js

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const countEl = document.getElementById("notes-count");
 const titleEl=document.getElementById("note-title");
 const descEl=document.getElementById("note-description");
 const saveBtn=document.getElementById("save-btn");
-const notesList=document.querySelectorAll(".note-list");
+const notesList=document.getElementById("note-list");
 const emptyEl=document.querySelector(".empty-state");
 
 let notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+let editing = false;
+
+let editingId= null;
 
 saveBtn.addEventListener("click", addNotes);
 
@@ -30,10 +34,20 @@ function addNotes() {
         return;
     }
 
+    if(editing){
+        const note =notes.find(n=> n.time ===editingId);
+        note.title=title;
+        note.description=description;
+        editing=false;
+        editingId=null;
+        saveBtn.innerText="Save Note";
+    }else{
     notes.push ( {
+        time: Date.now(),
         title: title,
         description: description
     });
+}
 
     titleEl.value = "";
     descEl.value = "";
@@ -54,3 +68,48 @@ function saveAndRender(){
     localStorage.setItem("notes", JSON.stringify(notes));
     renderNotes();
 }
+
+
+function renderNotes() {
+    notesList.innerText="";
+
+    notes.forEach( note => {
+        const li=document.createElement("li");
+        li.className="note-item";
+        li.innerHTML=`
+             <div class="note-content">
+                <h2 class="title">${note.title}</h2>
+                <p class="description">${note.description}</p>
+                <span class="time">${new Date(note.time).toLocaleString()}</span>
+                <button class="edit-btn"><i class="fas fa-edit"></i></button>
+                <button class="delete-btn"><i class="fas fa-trash"></i></button>
+            </div>
+        `;
+        
+        li.querySelector(".delete-btn").addEventListener("click", () => {
+            notes = notes.filter( n => n.time !== note.time);
+            saveAndRender();
+        });
+
+        li.querySelector(".edit-btn").addEventListener("click", () => {
+             titleEl.value = note.title;
+             descEl.value = note.description;
+             editing = true;
+             editingId = note.time;
+             saveBtn.innerText = "Update";
+             titleEl.focus();
+        });
+
+        
+        notesList.appendChild(li);
+    });
+
+  
+    updateCount();
+    showEmptyState();
+}
+
+
+
+renderNotes();
+
